f109dbbfc99dfed7ecb3540484e1499fb68097b5
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-08-08 11:37:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# pkgconfig
+
+`pkgconfig` is a Python module to interface with the `pkg-config` command line
+tool.
+
+It can be used to 
+
+- check if a package exists
+
+        >>> pkgconfig.exists('glib-2.0')
+        True
+
+- check if a package meets certain version requirements
+
+        >>> pkgconfig.installed('glib-2.0', '< 2.26')
+        False
+
+- query CFLAGS and LDFLAGS
+
+        >>> pkgconfig.cflags('glib-2.0')
+        '-I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include'
+
+        >>> pkgconfig.libs('glib-2.0')
+        '-lglib-2.0'
+
+
+The `pkgconfig` module is licensed under the MIT license.
---
+++ pkgconfig.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013 Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""pkgconfig is a Python module to interface with the pkg-config command line
+tool."""
+
+import subprocess
+import re
+
+
+def _compare_versions(v1, v2):
+    """
+    Compare two version strings and return -1, 0 or 1 depending on the equality
+    of the subset of matching version numbers.
+
+    The implementation is taken from the top answer at
+    http://stackoverflow.com/a/1714190/997768.
+    """
+    def normalize(v):
+        return [int(x) for x in re.sub(r'(\.0+)*$', '', v).split(".")]
+
+    return cmp(normalize(v1), normalize(v2))
+
+
+def _split_version_specifier(spec):
+    """Splits version specifiers in the form ">= 0.1.2" into ('0.1.2', '>=')"""
+    m = re.search(r'([<>=]?=?)?\s*((\d*\.)*\d*)', spec)
+    return m.group(2), m.group(1)
+
+
+def _query(package, option):
+    cmd = 'pkg-config {0} {1}'.format(option, package).split()
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, err = proc.communicate()
+    return out.rstrip()
+
+
+def exists(package):
+    """Return True if package information is available."""
+    try:
+        cmd = 'pkg-config --exists {0}'.format(package).split()
+        return subprocess.call(cmd) == 0
+    except OSError:
+        raise EnvironmentError("pkg-config not installed")
+
+
+def cflags(package):
+    """Return the CFLAGS string returned by pkg-config."""
+    return _query(package, '--cflags')
+
+
+def libs(package):
+    """Return the LDFLAGS string returned by pkg-config."""
+    return _query(package, '--libs')
+
+
+def installed(package, version):
+    """
+    Check if the package meets the required version.
+
+    The version specifier consists of an optional comparator (one of =, ==, >,
+    <, >=, <=) and an arbitrarily long version number separated by dots. The
+    should be as you would expect, e.g. for an installed version '0.1.2' of
+    package 'foo':
+
+    >>> installed('foo', '==0.1.2')
+    True
+    >>> installed('foo', '<0.1')
+    False
+    >>> installed('foo', '>= 0.0.4')
+    True
+    """
+    if not exists(package):
+        return False
+
+    number, comparator = _split_version_specifier(version)
+    modversion = _query(package, '--modversion')
+
+    try:
+        result = _compare_versions(modversion, number)
+    except ValueError:
+        msg = "{0} is not a correct version specifier".format(version)
+        raise ValueError(msg)
+
+    comparison_table = {
+        '>': result > 0,
+        '>=': result >= 0,
+        '': result == 0,
+        '=': result == 0,
+        '==': result == 0,
+        '<=': result <= 0,
+        '<': result < 0
+    }
+
+    return comparison_table[comparator]
